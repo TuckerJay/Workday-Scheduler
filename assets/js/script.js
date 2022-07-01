f88eae694b02deaad6c7edee1942e41890ae49e7
TuckerJD <tucker.j.dalrymple@gmail.com>
2022-06-30 21:46:30 -0500

    add save task functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,43 @@
-var timeStamp = document.querySelector("#currentDay");
-
+// Jumbotron timeblock
+var currentDay = document.querySelector("#currentDay");
 var time = moment().format('dddd MMMM Do');
+currentDay.textContent = time;
 
-timeStamp.textContent = time;
+var schedule = [];
+
+var loadStorage = function() {
+    schedule = JSON.parse(localStorage.getItem("schedule"));
+    for (var i = 0; i <schedule.length; i++) {
+        var btn = document.getElementById(i);
+
+        var textEl = $(btn).siblings(".description");
+        console.log(textEl);
+
+        var text = schedule[i];
+        console.log(text);
+
+        textEl.textContent = text;
+    }
+}
+
+var saveChange = function() {
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+
+}
+
+
+$(".saveBtn").on("click", function() {
+    var timeSlot = $(this).attr("id");
+    console.log(timeSlot);
+
+    var text = $(this).siblings(".description").val();
+    console.log(text);
+
+    schedule[timeSlot] = text;
+    console.log(schedule)
+
+    saveChange();
+})
+
+loadStorage();
+console.log(schedule)
